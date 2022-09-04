145bd993792d3ccb773640c5007c0f8a9056c6ea
H0und5 <84218198+H0und5@users.noreply.github.com>
2022-09-04 15:06:25 -0500

    dealing with multiple elements with v-model directive, using an array as the data structure.

--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -1,29 +1,57 @@
 <template>
   <form>
+
+    <!-- Account Information -->
+
     <label>Email:</label>
     <input type="email" required v-model="email" placeholder="Enter your email here">
 
     <label>Password:</label>
     <input type="password" required v-model="password" placeholder="Enter your password here">
 
+    <!-- Select Role -->
+    
     <label>Role:</label>
     <select v-model="role">
       <option value="" disabled hidden selected>Choose Role</option>
       <option value="Developer">Web Developer</option>
       <option value="Designer">Web Designer</option>
     </select>
-    
+
+    <!-- Names -->
+
+    <div>
+      <input type="checkbox" value="Tiffany" v-model="names"/>
+      <label>Tiffany</label>
+    </div>
+
+    <div>
+      <input type="checkbox" value="CommonProjects" v-model="names"/>
+      <label>Common Projects</label>
+    </div>
+
+
+    <div>
+      <input type="checkbox" value="Balenciaga" v-model="names"/>
+      <label>Balenciaga</label>
+    </div>
+
+    <!-- Accept Terms and Conditions -->
+
     <div class="terms">
-      <input type="checkbox" @click="termsClicked" required>
+      <input type="checkbox" v-model="terms" required>
       <label>Accept Terms and Conditions</label>
     </div>
+
+    <!-- Submit Button Handler -->
 
     <button type="submit" @click.prevent="submitFormHandler">Submit Form</button>
   </form>
 
-  <p>{{ email }}</p>
-  <p>{{ password }}</p>
-  <p>{{ role }}</p>
+  <p>Email: {{ email }}</p>
+  <p>Password: {{ password }}</p>
+  <p>Role: {{ role }}</p>
+  <p>Terms accepted: {{ terms }}</p>
 </template>
 
 <script>
@@ -34,10 +62,7 @@
         password: '',
         role: '',
         terms: false,
-
-        termsClicked() {
-          this.terms = !this.terms;
-        },
+        names: [],
 
         submitFormHandler() {
 
