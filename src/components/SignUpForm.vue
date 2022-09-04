c39875833bed241932bbe1e3276794216c5f6dcf
H0und5 <84218198+H0und5@users.noreply.github.com>
2022-09-04 14:09:21 -0500

    initial commit, learning more about how to structure component logic in a functional way

---
+++ src/components/SignUpForm.vue
@@ -0,0 +1,52 @@
+<template>
+  <form>
+    <label>Email:</label>
+    <input type="email" required v-model="email">
+
+    <button type="submit" @click.prevent="signUpHandler">Submit Form</button>
+  </form>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        email: '',
+        password: '',
+
+        signUpHandler() {
+          console.log(this.email);
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+  form {
+    max-width: 420px;
+    margin: 30px auto;
+    background: white;  
+    text-align: left;
+    padding: 40px;
+    border-radius: 10px;
+  }
+  label {
+    color: #aaa;
+    display: inline-block;
+    margin: 25px 0 15px;
+    font-size: 0.6em;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-weight: bold;
+  }
+  input {
+    display: block;
+    padding: 10px 6px;
+    width: 100%;
+    box-sizing: border-box;
+    border: none;
+    border-bottom: 1px solid #ddd;
+    color: #555;
+  }
+</style>
